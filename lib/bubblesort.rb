0660650038e02aa64a641d90a191ef8e5c1c10bf
anovik <aliaxandr.novik@mail.ru>
2016-11-12 22:27:00 +0100

    completed

--- lib/bubblesort.rb
+++ lib/bubblesort.rb
@@ -9,12 +9,14 @@
 #  end
 #  arr
 # end
+# return nil if arr.type_of?(Array) == false
 
 def bubblesort(arr)
+  return nil if arr.is_a?(Array) == false
   return arr if arr.length <= 1
   0.upto(arr.length - 1) do |i|
     0.upto(arr.length - 2 - i) do |j|
-      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j].to_s > arr[j + 1].to_s
     end
   end
   arr
--- spec/bubblesort_spec.rb
+++ spec/bubblesort_spec.rb
@@ -9,26 +9,54 @@ RSpec.describe 'bubblesort.rb' do
       expect { bubblesort([1, 3, 4]) }.not_to raise_error
     end
 
-    it 'returns sorted data' do
+    it 'returns sorted Integer data' do
       expect(bubblesort([5, 4, 3, 2, 1])).to match_array([1, 2, 3, 4, 5])
       expect(bubblesort([5, 3, 4, 1, 2])).to match_array([1, 2, 3, 4, 5])
       expect(bubblesort([4, 4, 1, 2, 1])).to match_array([1, 1, 2, 4, 4])
       expect(bubblesort([-5, 4, -3, 2, 1])).to match_array([-5, -3, 1, 2, 4])
       expect(bubblesort([1, 2, 3, 4, 5])).to match_array([1, 2, 3, 4, 5])
+    end
+
+    it 'returns sorted Float data' do
+      expect(bubblesort([4.3, 4.56, -2.6])).to match_array([-2.6, 4.3, 4.56])
+      expect(bubblesort([5.1, 4.3, 3.4, -2])).to match_array([-2, 3.4, 4.3, 5.1])
+    end
+
+    it 'returns sorted Numeric data' do
+      expect(bubblesort([4.3, 3, 4.56, -2.6])).to match_array([-2.6, 3, 4.3, 4.56])
+    end
+
+    it 'returns sorted String data' do
+      expect(bubblesort(['ba', 'a', 'aa', 'ab'])).to match_array(['a', 'aa', 'ab', 'ba'])
+    end
+
+    it 'returns sorted data' do
+      expect(bubblesort(['z', 4.56, 2.6, 'a'])).to match_array([2.6, 4.56, 'a', 'z'])
+    end
+
+    it 'should be contaned String collection' do
+      expect(bubblesort(%w(ba a aa ab))).to match_array(%w(aa a ab ba))
+    end
+
+    it 'length of output array' do
+      ary = [4, 7, 1, 5]
+      expect(bubblesort(ary).size).to equal(ary.size)
+    end
+
+    it 'empty array' do
       expect(bubblesort([])).to match_array([])
     end
 
     it 'returns the correct type' do
       expect(bubblesort([4, 3, 2, 1])).to be_an_instance_of Array
+      expect(bubblesort(%w(z q ba))).to be_an_instance_of Array
     end
 
-    it 'correct argument' do
-      arr = bubblesort([5.8, 7, 6, 4, 5, 1, 2, 3])
-      i = 0
-      arr.each do |x|
-        i += 1 if x.instance_of?(Numeric) != true
-      end
-      expect(i.zero?)
+    it 'when argument is not Array' do
+      expect(bubblesort(nil)).to be nil
+      expect(bubblesort('text')).to be nil
+      expect(bubblesort(111)).to be nil
+      expect(bubblesort(10.3)).to be nil
     end
   end
 end
